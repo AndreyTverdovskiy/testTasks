cc5e24a942ba8a124b594ef06af5e2c048413de2
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2022-03-06 14:17:47 +0300

    Refactored App component

--- omdb/src/App.tsx
+++ omdb/src/App.tsx
@@ -1,32 +1,90 @@
-import React, {ChangeEvent, useState} from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import s from './App.module.scss'
 import API from "./api/omdb-api";
+import {Film} from "./components/Film/Film";
+import {Paginator} from "./components/Paginator/Paginator";
+import Header from "./components/Header/Header";
+
+type searchResultType = {
+    Poster: string,
+    Title: string,
+    Type: string,
+    Year: string,
+    imdbID: string
+    totalResults: string
+}
 
 function App() {
+    const [searchResult, setSearchResult] = useState<Array<searchResultType>>([]);
     const [searchName, setSearchName] = useState('');
-    const [searchResult, setSearchResult] = useState('');
+    const [error, setError] = useState('')
 
-    const searchFilm = () => {
-        API.searchFilmsByTitle(searchName)
-            .then(res => {
-                console.log(res)
-            })
+    const [totalResults, setTotalResults] = useState('')
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const searchFilm = async () => {
+        try {
+            const {data} = await API.searchFilmsByTitle(searchName, currentPage);
+            const {Search, Error, Response, totalResults} = data;
+            if (Response === 'True') {
+                setSearchResult(Search)
+                setTotalResults(totalResults)
+                setError('')
+            } else {
+                setError(Error)
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    const onChangeSearchName = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearchName(e.currentTarget.value)
+    useEffect(() => {
+        searchFilm();
+    }, [searchName, currentPage])
+
+    const changePage = (currentPage: number) => {
+        setCurrentPage(currentPage)
     }
 
-    return (
-        <div className="App">
-            <h1>Movie Catalog</h1>
-            <input type='text' value={searchName} onChange={onChangeSearchName}/>
-            <button onClick={searchFilm}>Search</button>
+    const listFilms = searchResult.map(lf => {
+        return (
             <div>
-                {searchResult}
+                <Film poster={lf.Poster}
+                      name={lf.Title}
+                      year={lf.Year}
+                      imdbID={lf.imdbID}
+                      type={lf.Type}/>
             </div>
-        </div>
-    );
+        )
+    })
+
+    if (error !== 'Movie not found!') {
+        return (
+            <div className={s.App}>
+                <Header searchName={searchName}
+                        setSearchName={setSearchName}
+                />
+                <div className={s.films}>
+                    <span>You search for: {searchName}, {totalResults} results found</span>
+                    {listFilms}
+                </div>
+                <Paginator totalResults={+totalResults}
+                           onChangedPage={changePage}
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div className={s.App}>
+                <Header searchName={searchName}
+                        setSearchName={setSearchName}
+                />
+                <>
+                    {error}
+                </>
+            </div>
+        )
+    }
 }
 
 export default App;
